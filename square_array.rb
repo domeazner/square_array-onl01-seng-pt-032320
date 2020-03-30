f59102805d2c73cb80b1c0b3bcdd2e5c12cc224b
Dominc Carolan <dominiccarolan@gmail.com>
2020-03-30 20:55:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,12 @@
 #numbers = [1,2,3]
 
+#def square_array(array)
+#  array.each { |element| numbers << element ** 2 }
+#  numbers
+#end
+
 def square_array(array)
-  array.each { |element| numbers << element ** 2 }
-  numbers
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
